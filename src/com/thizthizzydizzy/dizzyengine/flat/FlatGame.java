01684bb06c778df95f761afc327706bcc28a17fa
Thiz <Thizthizzydizzy@gmail.com>
2024-10-09 21:31:33 -0400

    Added empty FlatGame layer

---
+++ src/com/thizthizzydizzy/dizzyengine/flat/FlatGame.java
@@ -0,0 +1,13 @@
+package com.thizthizzydizzy.dizzyengine.flat;
+import com.thizthizzydizzy.dizzyengine.DizzyLayer;
+public class FlatGame extends DizzyLayer{
+    @Override
+    public void init(){
+    }
+    @Override
+    public void render(double deltaTime){
+    }
+    @Override
+    public void cleanup(){
+    }
+}
