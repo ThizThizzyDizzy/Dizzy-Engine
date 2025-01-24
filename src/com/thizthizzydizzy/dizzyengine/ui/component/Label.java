76e25ebe21bf10d6926f07b5a6c2007cadb9941a
Thiz <Thizthizzydizzy@gmail.com>
2025-01-24 14:24:58 -0500

    Fixed label components having no preferred size

--- src/com/thizthizzydizzy/dizzyengine/ui/component/Label.java
+++ src/com/thizthizzydizzy/dizzyengine/ui/component/Label.java
@@ -1,6 +1,7 @@
 package com.thizthizzydizzy.dizzyengine.ui.component;
 import com.thizthizzydizzy.dizzyengine.ui.component.layer.ComponentLabel;
 import com.thizthizzydizzy.dizzyengine.ui.component.layer.ComponentLayer;
+import org.joml.Vector2f;
 public class Label extends Component{
     public ComponentLayer background = getUIContext().getDefaultComponentBackground(Label.class);
     public ComponentLabel label = getUIContext().getDefaultComponentLabel(Label.class);
@@ -13,5 +14,9 @@ public class Label extends Component{
     public void draw(double deltaTime){
         if(background!=null)background.draw(this, deltaTime);
         if(label!=null)label.draw(this, deltaTime);
+    }
+    @Override
+    public Vector2f getPreferredSize(){
+        return label.getPreferredSize();
     }
 }
