a2ca0021e19e81762a15219d8d8decb8051ea680
Thiz <Thizthizzydizzy@gmail.com>
2025-11-14 09:46:53 -0500

    Added headless mode via launch option

--- src/com/thizthizzydizzy/dizzyengine/DizzyEngine.java
+++ src/com/thizthizzydizzy/dizzyengine/DizzyEngine.java
@@ -39,11 +39,14 @@ public class DizzyEngine{
     public static boolean startMaximized = true;
 
     private static final ArrayList<Runnable> initFuncsGLFW = new ArrayList<>();
+    
+    public static boolean headless = false;
     public static void onInitGLFW(Runnable func){
         initFuncsGLFW.add(func);
     }
 
     public static void init(String title){
+        headless = Boolean.parseBoolean(System.getProperty("dizzyengine.headless", "false"));
         Logger.init();
         mainThread = Thread.currentThread();
         Thread.setDefaultUncaughtExceptionHandler((thread, ex) -> {
@@ -67,94 +70,99 @@ public class DizzyEngine{
             Logger.error("Uncaught Exception in Thread "+thread.getName()+":", ex);
         });
         Logger.push("INIT");
-        Logger.info("Initializing GLFW");
-        glfwSetErrorCallback(new GLFWErrorCallback(){
-            @Override
-            public void invoke(int err, long description){
-                Logger.error("GLFW ERROR "+err+": "+MemoryUtil.memUTF8(description));
-            }
-        });
-        if(!glfwInit())throw new RuntimeException("Failed to initialize GLFW!");
-        Logger.info("Initializing window");
-        //window
-        glfwWindowHint(GLFW_FOCUSED, GLFW_TRUE);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-        //multisampling //TODO graphics settings (MSAA)
-        glfwWindowHint(GLFW_STENCIL_BITS, 4);
-        glfwWindowHint(GLFW_SAMPLES, 4);
-        //openGL
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
-        initFuncsGLFW.forEach(Runnable::run);
+        if(headless){
+            Logger.info("Running headless - skipping window initialization");
+        }else{
+            Logger.info("Initializing GLFW");
+            glfwSetErrorCallback(new GLFWErrorCallback(){
+                @Override
+                public void invoke(int err, long description){
+                    Logger.error("GLFW ERROR "+err+": "+MemoryUtil.memUTF8(description));
+                }
+            });
+            if(!glfwInit())throw new RuntimeException("Failed to initialize GLFW!");
+            Logger.info("Initializing window");
+            //window
+            glfwWindowHint(GLFW_FOCUSED, GLFW_TRUE);
+            glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+            //multisampling //TODO graphics settings (MSAA)
+            glfwWindowHint(GLFW_STENCIL_BITS, 4);
+            glfwWindowHint(GLFW_SAMPLES, 4);
+            //openGL
+            glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+            glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+            glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+            glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+            initFuncsGLFW.forEach(Runnable::run);
 
-        Logger.info("Creating window");
-        window = glfwCreateWindow(1200, 700, title, 0, 0);
-        if(window==0){
-            glfwTerminate();
-            throw new RuntimeException("Failed to create GLFW window!");
+            Logger.info("Creating window");
+            window = glfwCreateWindow(1200, 700, title, 0, 0);
+            if(window==0){
+                glfwTerminate();
+                throw new RuntimeException("Failed to create GLFW window!");
+            }
+            if(startMaximized)glfwMaximizeWindow(window);
+            Logger.info("Initializing OpenGL");
+            glfwMakeContextCurrent(window);
+            glfwSwapInterval(1);//TODO graphics settings (VSync)
+            int[] w = new int[1];
+            int[] h = new int[1];
+            glfwGetFramebufferSize(window, w, h);
+            screenSize.set(w[0], h[0]);
+            glfwSetFramebufferSizeCallback(window, (window, width, height) -> {
+                if(window!=DizzyEngine.window)return;
+                screenSize.set(width, height);
+                windowSizeChanged = true;
+            });
+            GL.createCapabilities(true);
+            Logger.info("Initializing Render Engine");
+            glClearColor(0f, 0f, 0f, 0f);
+            glEnable(GL_MULTISAMPLE);
+            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+            glEnable(GL_BLEND);
+            glEnable(GL_STENCIL_TEST);
+            glEnable(GL_DEPTH_TEST);
+            glEnable(GL_CULL_FACE);
+            Logger.info("Initializing Elements");
+            Renderer.initElements();//TODO I don't like this
+            Renderer.setDefaultShader(Shader.loadInternal("vert.shader", "frag.shader"));
+            Logger.info("Initializing Layers");
+            synchronized(layers){
+                wrapEvent(layers, DizzyLayer::init);
+            }
+            Logger.info("Initializing Input");
+            glfwSetCharCallback(window, (window, codepoint) -> {
+                if(window==DizzyEngine.window)wrapEvent(layers, (layer) -> layer.onChar(0, codepoint));
+            });
+            glfwSetCharModsCallback(window, (window, codepoint, mods) -> {
+                if(window==DizzyEngine.window)wrapEvent(layers, (layer) -> layer.onCharMods(0, codepoint, mods));
+            });
+            glfwSetCursorEnterCallback(window, (window, entered) -> {
+                if(window==DizzyEngine.window)wrapEvent(layers, (layer) -> layer.onCursorEnter(0, entered));
+            });
+            glfwSetCursorPosCallback(window, (window, xpos, ypos) -> {
+                if(window==DizzyEngine.window)wrapEvent(layers, (layer) -> layer.onCursorPos(0, xpos, ypos));
+            });
+            glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+                if(window==DizzyEngine.window)wrapEvent(layers, (layer) -> layer.onKey(0, key, scancode, action, mods));
+            });
+            glfwSetMouseButtonCallback(window, (window, button, action, mods) -> {
+                if(window==DizzyEngine.window)wrapEvent(layers, (layer) -> layer.onMouseButton(0, button, action, mods));
+            });
+            glfwSetScrollCallback(window, (window, xoffset, yoffset) -> {
+                if(window==DizzyEngine.window)wrapEvent(layers, (layer) -> layer.onScroll(0, xoffset, yoffset));
+            });
+            glfwSetDropCallback(window, (window, count, names) -> {
+                if(window==DizzyEngine.window)wrapEvent(layers, (layer) -> layer.onDrop(0, count, names));
+            });
+            glfwSetJoystickCallback((jid, event) -> {
+                wrapEvent(layers, (layer) -> layer.onJoystick(0, jid, event));
+            });
         }
-        if(startMaximized)glfwMaximizeWindow(window);
-        Logger.info("Initializing OpenGL");
-        glfwMakeContextCurrent(window);
-        glfwSwapInterval(1);//TODO graphics settings (VSync)
-        int[] w = new int[1];
-        int[] h = new int[1];
-        glfwGetFramebufferSize(window, w, h);
-        screenSize.set(w[0], h[0]);
-        glfwSetFramebufferSizeCallback(window, (window, width, height) -> {
-            if(window!=DizzyEngine.window)return;
-            screenSize.set(width, height);
-            windowSizeChanged = true;
-        });
-        GL.createCapabilities(true);
-        Logger.info("Initializing Render Engine");
-        glClearColor(0f, 0f, 0f, 0f);
-        glEnable(GL_MULTISAMPLE);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-        glEnable(GL_BLEND);
-        glEnable(GL_STENCIL_TEST);
-        glEnable(GL_DEPTH_TEST);
-        glEnable(GL_CULL_FACE);
-        Logger.info("Initializing Elements");
-        Renderer.initElements();//TODO I don't like this
-        Renderer.setDefaultShader(Shader.loadInternal("vert.shader", "frag.shader"));
-        Logger.info("Initializing Layers");
-        synchronized(layers){
-            wrapEvent(layers, DizzyLayer::init);
-        }
-        Logger.info("Initializing Input");
-        glfwSetCharCallback(window, (window, codepoint) -> {
-            if(window==DizzyEngine.window)wrapEvent(layers, (layer) -> layer.onChar(0, codepoint));
-        });
-        glfwSetCharModsCallback(window, (window, codepoint, mods) -> {
-            if(window==DizzyEngine.window)wrapEvent(layers, (layer) -> layer.onCharMods(0, codepoint, mods));
-        });
-        glfwSetCursorEnterCallback(window, (window, entered) -> {
-            if(window==DizzyEngine.window)wrapEvent(layers, (layer) -> layer.onCursorEnter(0, entered));
-        });
-        glfwSetCursorPosCallback(window, (window, xpos, ypos) -> {
-            if(window==DizzyEngine.window)wrapEvent(layers, (layer) -> layer.onCursorPos(0, xpos, ypos));
-        });
-        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-            if(window==DizzyEngine.window)wrapEvent(layers, (layer) -> layer.onKey(0, key, scancode, action, mods));
-        });
-        glfwSetMouseButtonCallback(window, (window, button, action, mods) -> {
-            if(window==DizzyEngine.window)wrapEvent(layers, (layer) -> layer.onMouseButton(0, button, action, mods));
-        });
-        glfwSetScrollCallback(window, (window, xoffset, yoffset) -> {
-            if(window==DizzyEngine.window)wrapEvent(layers, (layer) -> layer.onScroll(0, xoffset, yoffset));
-        });
-        glfwSetDropCallback(window, (window, count, names) -> {
-            if(window==DizzyEngine.window)wrapEvent(layers, (layer) -> layer.onDrop(0, count, names));
-        });
-        glfwSetJoystickCallback((jid, event) -> {
-            wrapEvent(layers, (layer) -> layer.onJoystick(0, jid, event));
-        });
         Logger.pop();
     }
     public static void setWindowIcon(Image image){
+        if(headless)return;
         Logger.push("DizzyEngine");
         Logger.info("Loading window icon");
         try(GLFWImage.Buffer iconBuffer = GLFWImage.create(1); GLFWImage icon = GLFWImage.create()){
@@ -219,56 +227,65 @@ public class DizzyEngine{
             PerformanceTracker.reset();
             Logger.reset();
             Logger.push(DizzyEngine.class);
-            if(glfwWindowShouldClose(window)){
-                Logger.info("Window closed!");
-                running = false;
-                continue;
-            }
-            if(windowSizeChanged||screenBuffer==null){
-                windowSizeChanged = false;
-                if(screenBuffer!=null){
-                    screenBuffer.destroy();
+            if(DizzyEngine.headless){
+                try{
+                    Thread.sleep(10);
+                }catch(InterruptedException ex){
+                    Logger.warn("Interrupted! Shutting down...");
+                    stop();
                 }
-                screenBuffer = new Framebuffer(Math.max(1, screenSize.x), Math.max(1, screenSize.y));
-                synchronized(layers){
-                    wrapEvent(layers, (layer) -> layer.onScreenSize(screenSize));
+            }else{
+                if(glfwWindowShouldClose(window)){
+                    Logger.info("Window closed!");
+                    running = false;
+                    continue;
                 }
-                windowProjectionMatrix.setOrtho(0, screenSize.x, screenSize.y, 0, 0.1f, 10f);
-            }
-            //deltaTime
-            double time = glfwGetTime();
-            double deltaTime = lastFrame>-1?time-lastFrame:0;
-            lastFrame = time;
-            //reset rendering
-            screenBuffer.bind();
-            glViewport(0, 0, screenSize.x, screenSize.y);//TODO allow different screen size (letterboxed and/or VR)
-            //clear buffers
-            glClearColor(0f, 0f, 0f, 0f);
-            glStencilMask(0xff);
-            glClear(GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
-            glStencilMask(0x00);
-            glClearColor(0f, 0f, 0f, 0f);
-            glClear(GL_COLOR_BUFFER_BIT);
-            synchronized(layers){
-                wrapEvent(layers, (layer) -> {
-                    screenBuffer.bind();//just in case it was changed
-                    Renderer.reset();
-                    layer.render(deltaTime);
-                });
-                Renderer.reset();
-                glBindFramebuffer(GL_FRAMEBUFFER, 0);
-                
-                // Clear the main window before drawing; this is required for transparency
+                if(windowSizeChanged||screenBuffer==null){
+                    windowSizeChanged = false;
+                    if(screenBuffer!=null){
+                        screenBuffer.destroy();
+                    }
+                    screenBuffer = new Framebuffer(Math.max(1, screenSize.x), Math.max(1, screenSize.y));
+                    synchronized(layers){
+                        wrapEvent(layers, (layer) -> layer.onScreenSize(screenSize));
+                    }
+                    windowProjectionMatrix.setOrtho(0, screenSize.x, screenSize.y, 0, 0.1f, 10f);
+                }
+                //deltaTime
+                double time = glfwGetTime();
+                double deltaTime = lastFrame>-1?time-lastFrame:0;
+                lastFrame = time;
+                //reset rendering
+                screenBuffer.bind();
+                glViewport(0, 0, screenSize.x, screenSize.y);//TODO allow different screen size (letterboxed and/or VR)
+                //clear buffers
+                glClearColor(0f, 0f, 0f, 0f);
+                glStencilMask(0xff);
+                glClear(GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
+                glStencilMask(0x00);
                 glClearColor(0f, 0f, 0f, 0f);
                 glClear(GL_COLOR_BUFFER_BIT);
-                
-                glDisable(GL_CULL_FACE);
-                glDisable(GL_DEPTH_TEST);
-                Renderer.view(windowViewMatrix);
-                Renderer.projection(windowProjectionMatrix);
-                Renderer.fillRect(0, 0, screenSize.x, screenSize.y, screenBuffer.texture);//draw screen buffer to renderbuffer
-                glfwSwapBuffers(window);
-                glfwPollEvents();
+                synchronized(layers){
+                    wrapEvent(layers, (layer) -> {
+                        screenBuffer.bind();//just in case it was changed
+                        Renderer.reset();
+                        layer.render(deltaTime);
+                    });
+                    Renderer.reset();
+                    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+                    // Clear the main window before drawing; this is required for transparency
+                    glClearColor(0f, 0f, 0f, 0f);
+                    glClear(GL_COLOR_BUFFER_BIT);
+
+                    glDisable(GL_CULL_FACE);
+                    glDisable(GL_DEPTH_TEST);
+                    Renderer.view(windowViewMatrix);
+                    Renderer.projection(windowProjectionMatrix);
+                    Renderer.fillRect(0, 0, screenSize.x, screenSize.y, screenBuffer.texture);//draw screen buffer to renderbuffer
+                    glfwSwapBuffers(window);
+                    glfwPollEvents();
+                }
             }
         }
         Logger.info("Shutting down...");
