de3597ce8d248e0b7b9887ef3681c30ba66ac5f9
ThizThizzyDizzy <thizthizzydizzy@gmail.com>
2026-01-16 10:01:42 -0500

    gave preRender and postRender in Instancable a default method body

--- src/com/thizthizzydizzy/dizzyengine/graphics/batch/Instanceable.java
+++ src/com/thizthizzydizzy/dizzyengine/graphics/batch/Instanceable.java
@@ -4,8 +4,8 @@ import com.thizthizzydizzy.dizzyengine.world.object.WorldObject;
 public interface Instanceable{
     public boolean canInstance(WorldObject other);
     public Mesh getMesh();
-    public void preRender();
-    public void postRender();
+    public default void preRender(){}
+    public default void postRender(){}
     public default float[] getInstanceData(){
         return null;
     }
